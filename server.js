9b41e3667ad78d7ebb6d27219413fd62fb0cf3d0
eleegha <eleegha@gmail.com>
2020-12-09 19:16:44 +0000

    finished bug command

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 let Discord = require("discord.js")
+let Sequelize = require('sequelize')
 let client = new Discord.Client()
 
 const { prefix, token } = require('./config.json');
-var count = 0
+count = 1
+
+
+/*
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	// SQLite only
+	storage: 'database.sqlite',
+});
+*/
 
 var latest = "didnt work"
 client.on("message", message => {
@@ -33,6 +45,12 @@ client.on("message", message => {
       message.channel.send(`https://media.discordapp.net/attachments/783093399804313620/784470247477477407/unknown.png?width=608&height=144`)
       break;
 
+    case 'prune':
+      if(message.author.id == '<@656889527561879572>'){
+        message.channel.send('no')
+      }
+
+
     case 'test':
       if (message.member.hasPermission('KICK_MEMBERS')) {
         console.log('This member can kick');
@@ -60,14 +78,17 @@ client.on("message", message => {
         .setFooter("Footer")
       message.channel.send(embed)
       break;
-
+    
+    case 'avatar':
+      message.channel.send(message.author.displayAvatarURL(message.author))
+      break;
     
 
     case 'latest':
-      console.log("a")
-      var content = message.content.slice(prefix.length + command.length).trim().split('ifyoumanagetohavethisinalinkwow')
+      var content = message.content.slice(prefix.length + 'latest'.length).trim().split('cvbaghsnbfv aaaaasdfgaksjdhfvbwbna')
+      console.log(content)  
       //trims the !latest part off of the message
-      if (message.member.hasPermission(['ADMINISTRATOR']) && content.length >= 2) {//checks if a user has administrator perms
+      if (message.member.hasPermission(['ADMINISTRATOR']) && content.length == 1) {//checks if a user has administrator perms
         console.log(content+`b`)
         latest = content[1]
         message.channel.send(`Link updated`)
@@ -75,17 +96,11 @@ client.on("message", message => {
       else {
         message.channel.send(content)
       }
+      
       break;
 
     case "idea": 
 
-      var array = message.content.slice(prefix.length + command.length).trim().split(`,`)
-
-      if (!args.length) {
-        message.delete()
-        return message.channel.send(`You didn't provide any arguements, ${message.author}!`)
-
-      }
       let embed3 = new Discord.MessageEmbed()
         .setTitle(array[0])
         .setDescription(array[1])
@@ -115,32 +130,106 @@ client.on("message", message => {
       break;
 
     case 'bug':
+
+      message.delete(
+
+      )
+      message.reply('we have set you a DM with the report form.') 
+      count = 0
       const filter = m => !m.author.bot;
       let embed2 = new Discord.MessageEmbed()
-      embed2.setFooter(`Submitted by: ${message.author.tag}`)
-      message.channel.send(embed2)
+      embed2.setFooter(`Submitted by: ${message.author.tag}`,`${message.author.displayAvatarURL(message.author)}`)
       message.author.createDM().then(async(channel) => {
+        message.author.send('What is the name for your bug?')
         
+        const collector = await channel.createMessageCollector(filter, {max: 1, time: 60000});
+        collector.on('collect', async msg => {
+          const title = msg.content
+
+            //Gets the user to state the info about the bug
+            message.author.send("Please describe the bug here:")
+            const collector2 = await channel.createMessageCollector(filter, {max: 1, time: 60000});
+            collector2.on('collect', async msg => {
+              const bugInfo = msg.content
+
+              //Gets the user to state their OS Version
+              message.author.send("Please state your OS (Operating System) version here:")
+              const collector3 = await 
+              channel.createMessageCollector(filter, {max: 1, time: 60000});
+              collector3.on('collect', async msg => {
+                const osv = msg.content
+
+                //Gets the user to state their Browser Version
+                message.author.send("Please state you browser version here:")
+                const collector4 = await 
+               channel.createMessageCollector(filter, {max: 1, time: 60000});
+               collector4.on('collect', async msg => {
+                 const bv = msg.content
+
+                 //Gets the user to state how to reproduce the bug
+                 message.author.send("How to reproduce?")
+                 const collector5 = await 
+                channel.createMessageCollector(filter, {max: 1, time: 60000});
+                 collector5.on('collect', async msg => {
+                   const repro = msg.content
+
+                   //Forming the embed
+                   embed2.setTitle(title)
+                   embed2.addFields(
+                     { name: 'Bug Info', value:bugInfo },
+                     { name: 'How to reproduce', value:repro},
+                     { name: '\u200B', value: '\u200B' },
+                     { name: 'OS Version', value:osv , inline: true},
+                     { name: 'Browser Version', value:bv , inline: true},
+                   
+                     
+                   )
+                   embed2.setTimestamp(),
+                   embed2.setColor('GREEN')
+                   embed2.setThumbnail('https://cdn.discordapp.com/icons/773445116572073995/5a124198a617d8312bb9a205fd46304d.png?size=128')
+                   message.author.send('Are you sure you want to report this bug? `YES` or `NO`')
+                   message.author.send(embed2)
+
+                  const yes = ['yes','y','ye','yeah','yep','yepp']
+
+                   const confirm = await channel.createMessageCollector(filter, {max: 1, time: 30000, errors: ['time'] })
+                   confirm.on('collect', async msg => {
+                      if(yes.includes(msg.content.toLowerCase())) {
+                        message.channel.send(embed2)
+                      }
+                      else {
+                        console.log(msg.content)
+                        message.author.send('*Process Termiated*')
+                      }
+
+                   })
+                   })
+
+                 })
+               })
+
+
+              })
+
+            })
+          
+          
+          
+        })
         
-        while(count<5) {
-          if(count == 0) {
+          
+          
+       /*   if(count == 0) {
             message.author.send('woo')
-            const collector = channel.createMessageCollector(filter, {max: 1, time: 60000});
+            console.log("ee")
             collector.on('collect', msg =>{
               message.author.send(`${msg.content}`)
 
-            }).then( msg => {
-              message.author.send('woo2')
-              const collector = channel.createMessageCollector(filter, {max: 1, time: 60000});
-              collector.on('collect', msg =>{
-                message.author.send(`${msg.content}`)
-             })
-            }
-
-            )
+            })
+          }
 
             
-          }
+          
           else if(count == 1) {
             message.author.send('woo2')
             const collector = channel.createMessageCollector(filter, {max: 1, time: 60000});
@@ -151,8 +240,9 @@ client.on("message", message => {
           else {
             message.author.send("uh oh")
           }
-          count = count + 1
-       }})
+          */
+          
+       
       break;
         
         
